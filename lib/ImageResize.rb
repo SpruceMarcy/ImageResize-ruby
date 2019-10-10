ad1087d1e907af372b3dc4e9ecbb0fedce096d4c
Marcy <40660991+SpruceMarcy@users.noreply.github.com>
2019-10-10 23:53:43 +0100

    "couldn't find ImageResize/Image"

--- lib/ImageResize.rb
+++ lib/ImageResize.rb
@@ -1,7 +1,7 @@
 $:.unshift(File.dirname(__FILE__)) unless
   $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
 
-require 'ImageResize/Image'
+require 'ImageResize/Image.rb'
 
 module ImageResize
   VERSION = '0.0.5'
